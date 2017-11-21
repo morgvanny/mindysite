0ea9a1570b68037ecc60c6167340dae7b8b94afc
morgvanny <morgvanny@gmail.com>
2017-11-20 20:57:39 -0800

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar/>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Mindy Furano</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href ="#">Home</a>
+      <a href ="#">About</a>
+      <a href ="#">Check These Out</a>
+      <a href ="#">Featured On</a>
+      <a href ="#">Lists</a>
+      <a href ="#">Interviews</a>
+      <a href ="#">Contact</a>
+    </div>
+  );
+};
+
+export default Navbar;
